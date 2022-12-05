a75f112d2481a75a32efa4cda8581354c115c7aa
Leoluwa <leoluamid@gmail.com>
2022-12-06 00:40:18 +0100

    Multiple returns :)

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if length == 0:
+        return None
+    
+    max_value = my_list[0]
+
+    for u in range(1, length):
+        if my_list[u] > max_value:
+            max_value = my_list[u]
+    
+    return max_value
