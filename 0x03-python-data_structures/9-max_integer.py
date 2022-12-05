1b6447ad7975e79bcdbe83e7e5d8893a8ec20ee1
Leoluwa <leoluamid@gmail.com>
2022-12-06 00:47:46 +0100

    Multiple returns :)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,11 +4,11 @@ def max_integer(my_list=[]):
 
     if length == 0:
         return (None)
- 
+
     max_value = my_list[0]
 
     for u in range(1, length):
         if my_list[u] > max_value:
             max_value = my_list[u]
-    
+
     return (max_value)
