e61b564c8ccb1675a42e1f5cc3097d3ce279ec21
Leoluwa <leoluamid@gmail.com>
2022-12-05 23:35:30 +0100

    Add two tuples :)

---
+++ 0x03-python-data_structures/7-pseudocode.py
@@ -0,0 +1,31 @@
+
+
+        
+        length_a > 1
+        length_a == 0
+        length_a == 1
+
+        length_b > 1
+        length_b == 0
+        length_b == 1
+
+
+            Conditions :
+
+                (length_a > 1) and (length_b > 1)
+
+                (length_a > 1) and (length_b == 1)
+
+                (length_a > 1) and (length_b == 0)
+
+                (length_a == 0) and (length_b > 1)
+
+                (length_a == 0) and (length_b == 1)
+
+                (length_a == 0) and (length_b == 0)
+
+                (length_a == 1) and (length_b > 1)
+
+                (length_a == 1) and (length_b == 1)
+
+                (length_a == 1) and (length_b == 0)
