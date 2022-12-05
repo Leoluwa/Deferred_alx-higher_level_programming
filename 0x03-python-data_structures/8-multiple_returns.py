9cde95b4c22e739221dfdd2d917e98194cbe6ea3
Leoluwa <leoluamid@gmail.com>
2022-12-06 00:14:00 +0100

    Multiple returns :)

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
     length = len(sentence)
-    first_ch = sentence[0]
 
     if length == 0:
-        first_ch = None
-        return (length, first_ch)
+        return (length, None)
     else:
-        return (length, first_ch)
+        return (length, sentence[0])
